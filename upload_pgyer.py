fa97cb315a5bb1daf119c6d6313706c1168bfb8a
zhou <zhoutjcc@msn.cn>
2022-09-05 21:21:42 +0800

    支持蒲公英快速发布API

--- upload_pgyer.py
+++ upload_pgyer.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import requests
 import argparse
@@ -6,18 +7,62 @@ import argparse
 import json
 from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 
-upload_url = 'https://upload.pgyer.com/apiv1/app/upload'
+# _api_key去下面这个网站去获取
+# https://www.pgyer.com/account/api
+
+token_url = "https://www.pgyer.com/apiv2/app/getCOSToken"
+build_info_url = "https://www.pgyer.com/apiv2/app/buildInfo"
 
 
 class UploadOptions:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="自动上传文件")
         self.parser.add_argument("--path", type=str, required=True, help="apk的绝对路径，或者apk的根目录")
-        self.parser.add_argument("--uKey", type=str, required=True, help="uKey")
         self.parser.add_argument("--api_key", type=str, required=True, help="_api_key")
 
     def parse(self):
         return self.parser.parse_args()
+
+
+# 获取上传文件的token
+def getCOSToken(_api_key: str):
+    e = MultipartEncoder(
+        fields={
+            '_api_key': _api_key,
+            'buildType': "android"}
+    )
+    result_response = requests.post(token_url, data=e, headers={'Content-Type': e.content_type})
+    text = json.loads(result_response.text)
+    print("获取token的方式-->", result_response.status_code, text)
+    return text["data"]
+
+
+# 检查上传文件之后的发布进度
+def buildInfo(_api_key: str, build_key: str) -> bool:
+    e = MultipartEncoder(
+        fields={
+            '_api_key': _api_key,
+            'buildKey': build_key}
+    )
+    build_info_response = requests.post(build_info_url, data=e, headers={'Content-Type': e.content_type})
+
+    data = json.loads(build_info_response.text)
+    code = data["code"]
+    if code == 1216:
+        print("发布失败-->", build_info_response.text)
+        return True
+    elif code == 0:
+        print("发布成功-->", build_info_response.text)
+        return True
+    else:
+        print("发布中-->", build_info_response.text)
+        time.sleep(5)
+        return buildInfo(_api_key, build_key)
+
+
+# MultipartEncoderMonitor的进度反馈
+def my_callback(monitor):
+    print('\r', '上传进度：', monitor.bytes_read / monitor.len, end='', flush=True)
 
 
 # 使用的是python3，需要pip3 install requests requests_toolbelt
@@ -41,33 +86,30 @@ if __name__ == '__main__':
 
     for path in paths:
         print("开始上传", path)
-
-        # uKey和_api_key去下面这个网站去获取
-        # https://www.pgyer.com/doc/api#uploadApp
+        token_response = getCOSToken(args.api_key)
+        # 参考文档
+        # https://www.pgyer.com/doc/view/api#fastUploadApp
         e = MultipartEncoder(
-            fields={'uKey': args.uKey,
-                    '_api_key': args.api_key,
-                    'file': (os.path.basename(path), open(path, 'rb'), 'application/octet-stream')}
+            fields={
+                'key': token_response["key"],
+                'signature': token_response["params"]["signature"],
+                'x-cos-security-token': token_response["params"]["x-cos-security-token"],
+                'file': (os.path.basename(path), open(path, 'rb'), 'application/octet-stream')}
         )
-
-
-        def my_callback(monitor):
-            print('\r', '上传进度：', monitor.bytes_read / monitor.len, end='', flush=True)
-
 
         print(e)
         m = MultipartEncoderMonitor(e, my_callback)
-        response = requests.post(upload_url, data=m, headers={'Content-Type': m.content_type})
-
+        response = requests.post(token_response["endpoint"], data=m, headers={'Content-Type': m.content_type})
         # 如果不用带进度，可以用下面的这个方法
-        # response = requests.post(upload_url, data={
-        #     'uKey': args.uKey,
-        #     '_api_key': args.api_key,
-        # }, files={'file': open(path, 'rb')})
+        # response = requests.post(token_response["endpoint"], data=e, headers={'Content-Type': e.content_type})
 
         response.encoding = "utf-8"
         print('\n', response.request.url, response.request.method)
         print(response.request.headers)
+        if response.status_code == 204:
+            print("上传成功-->")
+        else:
+            print("上传失败-->")
 
-        print("返回的内容为-->", response.status_code, json.loads(response.text))
+        buildInfo(args.api_key, token_response["key"])
         print('===================>')
